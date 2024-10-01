ec46bbe483a05d400395b4ab2f0536391a1fa41b
bar23354 <bar23354@uvg.edu.gt>
2024-10-01 09:28:23 -0600

    Cambios finales MatchScreen

--- app/src/main/java/com/example/match/MainActivity.kt
+++ app/src/main/java/com/example/match/MainActivity.kt
@@ -1,208 +1,102 @@
-package com.example.match
+package com.example.proyecto_plataformasmoviles
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.match.ui.theme.MatchTheme
-import com.example.proyecto_plataformasmoviles.R
-
-//Puntos a trabajar:
-//El espacio entre MATCH! y el boton de siguiente debe ser menor y deben estar relativamente cera
-//Los nombres de ambos perros deben de estar arriba de las imagenes redondeadas de perfil curbadas como un arcoiris
-//El Texto de "Yorki: Milo, Pug: Lucas y Beagle: Tommy... también revisaron tu perfil" debe estar más abajo casi tocando el fondo de la pantalla con el fondo blancod e las nubes para que contraste bien con el color negro de su texto
-
-
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.proyecto_plataformasmoviles.ui.theme.Proyecto_plataformasMovilesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            DogMatchScreen()
+            Proyecto_plataformasMovilesTheme {
+                App()
+            }
         }
     }
 }
 
+
 @Composable
-fun DogMatchScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.Fondo))
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(0.dp),
-            verticalArrangement = Arrangement.SpaceBetween
+fun App(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    // Get current back stack entry
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    // Get the name of the current screen
+    val currentScreen = backStackEntry?.destination?.route ?: "screen1"
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "Registro",
+            modifier = modifier.padding(innerPadding)
         ) {
-            LeahProfileSection()
-            MatchSection()
-            NextButtonAndClouds()
-        }
-    }
-}
+            /*composable(route = "Screen1") { //inicio de sesión
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Screen1(
+                        innerPadding = PaddingValues(),
+                        navController
+                    )
+                }
+            }*/
 
-@Composable
-fun LeahProfileSection() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .padding(top = 16.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.leahpfp),
-            contentDescription = "Leah's Profile Picture",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .background(Color(0xFFb8a0e8))
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(
-            text = "Leah",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-    }
-}
-
-@Composable
-fun MatchSection() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 30.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            CircularProfile("Leah, Westie", R.drawable.leahpfp)
-            CircularProfile("Oliverio, Westie", R.drawable.oliveropfp)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Box(modifier = Modifier.padding(top = 18.dp)) { //Distancia entre dos circulos y MATCH {
-            Text(
-                text = "MATCH!",
-                fontSize = 66.sp,
-                fontWeight = FontWeight.Bold,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier
-                    .offset(x = 6.dp, y = 6.dp) // Adjust shadow position
-                    .background(Color.Black, shape = RoundedCornerShape(12.dp))
-                    .padding(10.dp)
-            )
-
-            Text(
-                text = "MATCH!",
-                fontSize = 66.sp,
-                fontWeight = FontWeight.Bold,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier
-                    .background(Color.White, shape = RoundedCornerShape(12.dp))
-                    .padding(10.dp)
-            )
-        }
-    }
-}
-
-@Composable
-fun CircularProfile(name: String, imageRes: Int) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = imageRes),
-            contentDescription = null,
-            modifier = Modifier
-                .size(150.dp)
-                .clip(CircleShape)
-                .background(colorResource(id = R.color.circlePurple))
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = name,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-    }
-}
-
-@Composable
-fun NextButtonAndClouds() {
-    Box(modifier = Modifier.fillMaxWidth()) {
-        Column(modifier = Modifier.align(Alignment.BottomCenter)) {
-            //"Siguiente"
-            Button(
-                onClick = { /* a noti o chat */ },
-                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.petPurple)),
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .fillMaxWidth(0.7f)
-                    .clip(RoundedCornerShape(16.dp))
-            ) {
-                Text(
-                    text = "Siguiente",
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 32.sp
-                )
+            composable(route = "Registro") { //registro
+                Registro(innerPadding, navController)
             }
 
-            Spacer(modifier = Modifier.height(0.dp))
+            /*composable(route = "Screen3") { //notificaciones
+                ThirdActivity(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center), navController)
+            }
 
-            Image(
-                painter = painterResource(id = R.drawable.clouds),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(160.dp), //altura nubes
-                contentScale = ContentScale.Crop
-            )
+            composable(route = "Registro") { //para ti
+                Registro(innerPadding, navController)
+            }*/
+
+            composable(route = "Perfil") {
+                CenterAlignedTopAppBar_Perfil(navController)
+            }
+
+            composable(route = "Recomendaciones") {
+                RecomendacionesScreen(innerPadding, navController)
+            }
+
+            /*composable(route = "Screen4") { //chat
+                CenterAlignedTopAppBar2(navController)
+            }
+
+            composable(route = "Screen4") { //match
+                CenterAlignedTopAppBar2(navController)
+            }
+
+            composable(route = "Screen4") { //visualización de los matches
+                CenterAlignedTopAppBar2(navController)
+            }*/
         }
-
-        Text(
-            text = "Yorki: Milo, Pug: Lucas y Beagle: Tommy... también revisaron tu perfil",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier
-                .padding(bottom = 0.dp)
-                .align(Alignment.Center)
-        )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DogMatchScreen()
 }
